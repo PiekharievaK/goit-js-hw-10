3fb852b2cf51cad2d9ff229b47356a3df0048f85
PiekharievaK <pechareva@gmail.com>
2021-12-08 00:11:06 +0200

    добавляет решение задания

---
+++ src/fetchCountries.js
@@ -0,0 +1,12 @@
+export default fetchCountries;
+   
+  function  fetchCountries(url) {
+           return fetch(url)
+            .then(response => {
+              if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+            })
+            .then(countries => { return countries })
+    }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
+import Notiflix from 'notiflix';
 import './css/styles.css';
+import CountriesList from './templates/countriesListCard'
+import CountryCard from './templates/countryCard.hbs'
+import FetchApi from './fetchCountries';
+import debounce  from 'lodash.debounce';
 
+const BASE_URL = 'https://restcountries.com/v3.1/name/';
+const URL_PARAMS = `name,capital,population,flags,languages`;
 const DEBOUNCE_DELAY = 300;
+const countryList = document.querySelector(`.country-list`);
+const findField = document.querySelector(`#search-box`)
+
+findField.addEventListener(`input`,  debounce(onFindfield, DEBOUNCE_DELAY))
+
+function onFindfield(evt) {
+    evt.preventDefault();
+    const name = evt.target.value;
+    const countryUrl = `${BASE_URL}${name}?fields=${URL_PARAMS}`
+
+    if (name.trim() === '') {
+        countryList.innerHTML = '';
+        return
+    };
+        
+    FetchApi(countryUrl)
+        .then(countries => { return countries })
+        .then(createMarkup)
+        .catch(onReject)
+};
+
+function createMarkup(countriesArr) {
+     
+        if (countriesArr.length === 1) {
+            countryList.innerHTML = `${CountryCard(countriesArr[0])}`;
+        }
+        if (countriesArr.length > 1 && countriesArr.length < 10) {
+             countryList.innerHTML ='';
+            countriesArr.forEach(element => {
+             countryList.insertAdjacentHTML(`beforeend`, CountriesList(element))
+          });          
+        }
+        if (countriesArr.length > 10) {
+            Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+        }
+};
+
+function onReject() {
+    Notiflix.Notify.failure(`Oops, there is no country with that name`)
+};
